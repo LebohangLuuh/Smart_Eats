091d9a470afae9008d89848297130284626aa4a2
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-06-18 21:38:58 +0200

    register page and the loader component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 export const routes: Routes = [
 { path: 'login', component: LoginComponent },
-{ path: 'register', component:RegisterComponent}
+{ path: 'register', component:RegisterComponent},
+{ path: 'loader', component:LoaderComponent}
+
 ];
